f7052d13aa31efd96fa9bda95ab35435e459a145
nikaNGC224 <veronika_rassokhina@mail.ru>
2024-04-04 21:13:42 +0300

    Initial project
    https://start.spring.io/#!type=gradle-project&language=java&platformVersion=3.2.4&packaging=jar&jvmVersion=17&groupId=com.poly&artifactId=authentication&name=authentication&description=Project%20for%20Polykill&packageName=com.poly.authentication&dependencies=web,data-jpa,testcontainers,security,devtools

---
+++ back/authentication/src/test/java/com/poly/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
