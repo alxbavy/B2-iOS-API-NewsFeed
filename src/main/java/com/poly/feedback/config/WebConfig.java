d94dbf3d66e81556f830052f23c8b63b33e3a943
Huyuhiu <tthhhuy7@gmail.com>
2024-05-02 23:28:39 +0700

    swagger

---
+++ src/main/java/com/poly/feedback/config/WebConfig.java
@@ -0,0 +1,33 @@
+package com.poly.feedback.config;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.util.unit.DataSize;
+import org.springframework.web.servlet.config.annotation.*;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowCredentials(false)
+                .allowedOriginPatterns("http://*","https://*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(1800).exposedHeaders("Authorization,Link,X-Total-Count,X-${jhipster.clientApp.name}-alert,X-${jhipster.clientApp.name}-error,X-${jhipster.clientApp.name}-params");
+
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+}
